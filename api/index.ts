0d227e24d3a7ff61e1d22a0171d12c950b223de6
amir ali <amir.ali16610@gmail.com>
2025-08-14 13:11:32 +0500

    Delete api directory

--- api/index.ts
+++
@@ -1,5 +0,0 @@
-// ❌ Old (won't work on Vercel)
-fetch("http://localhost:5000/expenses")
-
-// ✅ New (works from anywhere)
-fetch("https://af3c07d4-752f-4485-9b15-c19e3145c10b-00-2yfziis2nbhf0.riker.replit.dev/expenses")
